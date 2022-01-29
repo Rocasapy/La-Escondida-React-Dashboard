3a6cc55026b6dce9a7630df87704963352cc1d4a
Rocio Caballero <drcaballero25@gmail.com>
2022-01-29 14:49:07 -0300

    first commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Dashboard (props){
+    return (
+        <div>
+            Soy el dashboard de la Escondida 
+        </div>
+    );
+}
+export default  Dashboard;
